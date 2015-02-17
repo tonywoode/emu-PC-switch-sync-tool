05e7ccba1fba62ee2ca70777cdecd7be01a32188
tony woode <tonywoodecode@gmail.com>
2015-02-17 21:55:52 +0000

    tidyup Frontend folder

--- RunFrontend_and_ReplaceConfigs/ReplaceTextAndLaunchQP.bat
+++ RunFrontend_and_ReplaceConfigs/ReplaceTextAndLaunchQP.bat
@@ -1,12 +1,11 @@
-::@echo off & 
-SETLOCAL
+@echo off & SETLOCAL
 ::We will replace emu ini text with the args you pass to me, and launch qp
 ::So args are
 ::%1 = new Machine
 ::%2 = old width	%3 = new width
 ::%4 = old height	%4 = new height
 ::%6 = old refresh	%7 = new refresh
-::Bear in mind command line seems to be one based on this, where powershell is zero based (nb: that's because arg 0 is the command that was run in cmd, and c++ for that matter, right) 
+::note one based, where powershell is zero based (because arg 0 is the command that was run) 
 
 ::now powershell - remember we do NOT need old machine for this.....
 powershell -file .\..\replace_ini_list.ps1 %1 %2 %3 %4 %5 %6 %7
