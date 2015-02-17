44a5b437b1c618d7db9190a42a867df1170f8aea
tony woode <tonywoodecode@gmail.com>
2015-02-17 22:21:20 +0000

    Removed all calls to MACHINE_A's specs
    We don't need them anymore, we only look forward

--- RunFrontend_and_ReplaceConfigs/ReplaceTextAndLaunchQP.bat
+++ RunFrontend_and_ReplaceConfigs/ReplaceTextAndLaunchQP.bat
@@ -2,13 +2,13 @@
 ::We will replace emu ini text with the args you pass to me, and launch qp
 ::So args are
 ::%1 = new Machine
-::%2 = old width	%3 = new width
-::%4 = old height	%4 = new height
-::%6 = old refresh	%7 = new refresh
+::%2 = new width
+::%3 = new height
+::%4 = new refresh
 ::note one based, where powershell is zero based (because arg 0 is the command that was run) 
 
 ::now powershell - remember we do NOT need old machine for this.....
-powershell -file .\..\replace_ini_list.ps1 %1 %2 %3 %4 %5 %6 %7
+powershell -file .\..\replace_ini_list.ps1 %1 %2 %3 %4
 
 
 
