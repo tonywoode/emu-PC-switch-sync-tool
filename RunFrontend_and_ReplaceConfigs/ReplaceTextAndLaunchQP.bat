28b80371315150479debefbcbfa0adf898a3ff02
tony woode <tonywoodecode@gmail.com>
2015-02-17 18:45:26 +0000

    doc and paths

--- RunFrontend_and_ReplaceConfigs/ReplaceTextAndLaunchQP.bat
+++ RunFrontend_and_ReplaceConfigs/ReplaceTextAndLaunchQP.bat
@@ -8,12 +8,9 @@ SETLOCAL
 ::%6 = old refresh	%7 = new refresh
 ::Bear in mind command line seems to be one based on this, where powershell is zero based (nb: that's because arg 0 is the command that was run in cmd, and c++ for that matter, right) 
 
-::these get funny when called as admin so I had to cd to scripts dir
-cd ./../
 ::now powershell - remember we do NOT need old machine for this.....
-powershell -file P:\Scripts\Sync\SCRIPTS\replace_ini_list.ps1 %1 %2 %3 %4 %5 %6 %7
+powershell -file .\..\replace_ini_list.ps1 %1 %2 %3 %4 %5 %6 %7
 
-::we don't do the 2 files list, it would fuck it up
 
 
 
