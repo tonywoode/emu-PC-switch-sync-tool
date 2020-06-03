603306b517827738c0a9b04ed28f68723d56d05d
tony woode <tonywoodecode@gmail.com>
2020-06-04 00:01:13 +0100

    [nas sync] implement async savegame sync on qp runner...
    on start and exit, run freefilesync with a batch job that syncs
    potential savegames to the nas (facilitates playing games on multiple
    machines)
    
    * run it async with start, don't block qp starting
    * note the duplication in runner.bat, can i call 'call' within start
    and have it still be async? needs testing, then can write fn/subroutine
    call
    * also made calls to replace list async but in current window, realised
      a nice touch of being able to call without the flag and see console
      output persistently
    * start with freefilesync not realtime sync: we don't want to wait for a
      folderchange before starting

--- RunFrontend_and_ReplaceConfigs/runner.bat
+++ RunFrontend_and_ReplaceConfigs/runner.bat
@@ -2,10 +2,16 @@
 ::cd to script directory, for administrator needs to run this
 cd /D "%~dp0" 
 ::import the gamebase reg REG IMPORT "P:\GAMEBASE\Gamebase.reg"
-if "%computername%"=="RIVER" (call ReplaceTextAndLaunchQP.bat RIVER 3840 2160 60)
-if "%computername%"=="TRICKLE" (call ReplaceTextAndLaunchQP.bat TRICKLE 1366 768 60)
-if "%computername%"=="LAGOON" (call ReplaceTextAndLaunchQP.bat LAGOON 1280 800 60)
-if "%computername%"=="POND"  (call ReplaceTextAndLaunchQP.bat POND 2560 1600 60)
+::TODO: why did i comment that out?
+
+::here START ensures these get called async, a nice touch tho is remove the /B and you see the output in a new persisting window
+if "%computername%"=="RIVER"   (START /B ReplaceTextAndLaunchQP.bat RIVER 3840 2160 60)
+if "%computername%"=="TRICKLE" (START /B ReplaceTextAndLaunchQP.bat TRICKLE 1366 768 60)
+if "%computername%"=="LAGOON"  (START /B ReplaceTextAndLaunchQP.bat LAGOON 1280 800 60)
+if "%computername%"=="POND"    (START /B ReplaceTextAndLaunchQP.bat POND 2560 1600 60)
+
+::ensure we sync save games
+START "C:\Program Files\FreeFileSync\FreeFileSync.exe" "P:\WinScripts\Emulator_PC_Switcher_Sync_Tool\RealtimeSync_with_FreeFileSync\Emulator_NAS_Sync\1.Save_Game_sync.ffs_batch"
 
 ::my Emulators and frontend all live on Drive P (subst), so if we aren't on that drive, we won't be able to CD
 if not ("%~d0")==("P:") (P:)
@@ -19,6 +25,10 @@ start /D "P:\JoytoKey\" JoyToKey.exe
 ::if we don't CD to qp's dir, realative paths won't work. Many tools currently need relative paths
 cd /D P:\QUICKPLAY\QUickPlayFrontend\qp 
 QP.exe
+
+::now we're done re-ensure we sync save games
+START "C:\Program Files\FreeFileSync\FreeFileSync.exe" "P:\WinScripts\Emulator_PC_Switcher_Sync_Tool\RealtimeSync_with_FreeFileSync\Emulator_NAS_Sync\1.Save_Game_sync.ffs_batch"
+
 ::export the gamebase reg before we close
 REG EXPORT HKEY_CURRENT_USER\Software\GB64 "P:\GAMEBASE\Gamebase.reg" /y
 
